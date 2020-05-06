869368b11aa31fc07f82ba4635ca5454cdcb9744
Jeffrey Valdez <53051337+jvaldezdipre@users.noreply.github.com>
2020-05-06 18:39:34 -0400

    Completed MVP

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
-  const toggleMode = e => {
-    e.preventDefault();
-    setDarkMode(!darkMode);
-  };
+const Navbar = ({ darkMode, toggleMode }) => {
   return (
-    <nav className="navbar">
+    <nav className='navbar'>
       <h1>Crypto Tracker</h1>
-      <div className="dark-mode__toggle">
+      <div className='dark-mode__toggle'>
         <div
           onClick={toggleMode}
           className={darkMode ? 'toggle toggled' : 'toggle'}
--- src/index.js
+++ src/index.js
@@ -1,30 +1,33 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import { useDarkMode } from './hooks/useDarkMode';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-import Charts from "./components/Charts";
-import Navbar from "./components/Navbar";
+import Charts from './components/Charts';
+import Navbar from './components/Navbar';
 
-import "./styles.scss";
+import './styles.scss';
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
+  const [darkMode, toggleMode] = useDarkMode(false);
 
   useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true'
       )
       .then(res => setCoinData(res.data))
       .catch(err => console.log(err));
   }, []);
+
   return (
-    <div className="App">
-      <Navbar />
+    <div className={`App${darkMode ? ' dark-mode' : ''}`}>
+      <Navbar darkMode={darkMode} toggleMode={toggleMode} />
       <Charts coinData={coinData} />
     </div>
   );
 };
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
